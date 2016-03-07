f1c78517ebf6a9e357d914a9f9020d27166dc61c
Simon <simon_petersen1994@hotmail.com>
2016-03-07 15:56:56 +0100

    DTO-klasse foreløbigt færdige.

--- src/dtu/rejseafregning/shared/BilagDTO.java
+++ src/dtu/rejseafregning/shared/BilagDTO.java
@@ -1,9 +1,34 @@
 package dtu.rejseafregning.shared;
 
+import java.io.File;
 import java.io.Serializable;
 
 public class BilagDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	private int bilagID;
+	private File billede;
+	
+	public BilagDTO() {}
+	
+	public BilagDTO(int bilagID, File billede) {
+		setBilagID(bilagID);
+		setBillede(billede);
+	}
 
+	public int getBilagID() {
+		return bilagID;
+	}
+
+	public void setBilagID(int bilagID) {
+		this.bilagID = bilagID;
+	}
+
+	public File getBillede() {
+		return billede;
+	}
+
+	public void setBillede(File billede) {
+		this.billede = billede;
+	}
 }
--- src/dtu/rejseafregning/shared/UdgiftDTO.java
+++ src/dtu/rejseafregning/shared/UdgiftDTO.java
@@ -1,9 +1,69 @@
 package dtu.rejseafregning.shared;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class UdgiftDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	private int udgiftID, bilagID;
+	private String udgiftType, betalingType, forklaring;
+	private Date dato;
+	
+	public UdgiftDTO() {}
+	
+	public UdgiftDTO(int udgiftID, int bilagID, String udgiftType, String betalingType, String forklaring, Date dato)
+	{
+		setUdgiftID(udgiftID);
+		setBilagID(bilagID);
+		setUdgiftType(udgiftType);
+		setBetalingType(betalingType);
+		setForklaring(forklaring);
+		setDato(dato);
+	}
+	
+	public int getUdgiftID() {
+		return udgiftID;
+	}
+	public void setUdgiftID(int udgiftID) {
+		this.udgiftID = udgiftID;
+	}
+	public int getBilagID() {
+		return bilagID;
+	}
+	public void setBilagID(int bilagID) {
+		this.bilagID = bilagID;
+	}
 
+	public String getUdgiftType() {
+		return udgiftType;
+	}
+
+	public void setUdgiftType(String udgiftType) {
+		this.udgiftType = udgiftType;
+	}
+
+	public String getBetalingType() {
+		return betalingType;
+	}
+
+	public void setBetalingType(String betalingType) {
+		this.betalingType = betalingType;
+	}
+
+	public String getForklaring() {
+		return forklaring;
+	}
+
+	public void setForklaring(String forklaring) {
+		this.forklaring = forklaring;
+	}
+
+	public Date getDato() {
+		return dato;
+	}
+
+	public void setDato(Date dato) {
+		this.dato = dato;
+	}
 }
