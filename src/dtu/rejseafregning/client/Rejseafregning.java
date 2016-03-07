b0b4aed5b9a8285ca661e1007b402ae94a8f8664
Simon <simon_petersen1994@hotmail.com>
2016-03-07 13:59:13 +0100

    Tilføjet MainView-klasse.

--- src/dtu/rejseafregning/client/Rejseafregning.java
+++ src/dtu/rejseafregning/client/Rejseafregning.java
@@ -1,6 +1,9 @@
 package dtu.rejseafregning.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+import dtu.rejseafregning.client.ui.MainView;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -8,6 +11,7 @@ import com.google.gwt.core.client.EntryPoint;
 public class Rejseafregning implements EntryPoint {
 		
 	public void onModuleLoad() {
-			
+//		MainView mView = new MainView();
+//		RootPanel.get().add(mView);
 	}
 }
---
+++ src/dtu/rejseafregning/client/ui/MainView.java
@@ -0,0 +1,11 @@
+package dtu.rejseafregning.client.ui;
+
+import com.google.gwt.user.client.ui.Composite;
+
+public class MainView extends Composite {
+	
+	public MainView() {
+		
+	}
+
+}
