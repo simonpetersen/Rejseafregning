b04f1243bd7e43384ba825e6613363e2d3461b66
Simon <simon_petersen1994@hotmail.com>
2016-03-07 15:34:00 +0100

    Tilføjet variable til DTO'ere.

--- src/dtu/rejseafregning/server/dal/RejsedagDTO.java
+++ src/dtu/rejseafregning/server/dal/RejsedagDTO.java
@@ -1,9 +1,60 @@
 package dtu.rejseafregning.server.dal;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class RejsedagDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	private boolean morgenmad, frokost, aftensmad;
+	private Date start, slut;
+	
+	public RejsedagDTO(boolean morgenmad, boolean frokost, boolean aftensmad, Date start, Date slut) {
+		setMorgenmad(morgenmad);
+		setFrokost(frokost);
+		setAftensmad(aftensmad);
+		setStart(start);
+		setSlut(slut);
+	}
+
+	public boolean harMorgenmad() {
+		return morgenmad;
+	}
+
+	public void setMorgenmad(boolean morgenmad) {
+		this.morgenmad = morgenmad;
+	}
+
+	public boolean harFrokost() {
+		return frokost;
+	}
+
+	public void setFrokost(boolean frokost) {
+		this.frokost = frokost;
+	}
+
+	public boolean harAftensmad() {
+		return aftensmad;
+	}
+
+	public void setAftensmad(boolean aftensmad) {
+		this.aftensmad = aftensmad;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getSlut() {
+		return slut;
+	}
+
+	public void setSlut(Date slut) {
+		this.slut = slut;
+	}
 
 }
