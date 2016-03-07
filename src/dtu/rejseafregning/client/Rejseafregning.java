a012d91daa4fb14449d512ca8598a8e60ed4c5ad
Simon <simon_petersen1994@hotmail.com>
2016-03-07 13:55:13 +0100

    Første commit.

---
+++ src/dtu/rejseafregning/client/Rejseafregning.java
@@ -0,0 +1,13 @@
+package dtu.rejseafregning.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Rejseafregning implements EntryPoint {
+		
+	public void onModuleLoad() {
+			
+	}
+}
