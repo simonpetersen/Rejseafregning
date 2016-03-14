b688893a77778da9934bff26663d7b347fda7b6e
Simon <simon_petersen1994@hotmail.com>
2016-03-14 16:11:49 +0100

    Push

--- src/dtu/rejseafregning/client/logic/Controller.java
+++ src/dtu/rejseafregning/client/logic/Controller.java
@@ -1,5 +1,27 @@
 package dtu.rejseafregning.client.logic;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.SimpleEventBus;
+import com.google.web.bindery.event.shared.binder.EventBinder;
+
+import dtu.rejseafregning.client.ui.MainView;
+
 public class Controller {
 
+	private MainView mainView;
+	
+	private EventBus eventBus;
+	
+	interface MyEventBinder extends EventBinder<Controller> {}
+	private final MyEventBinder eventBinder = GWT.create(MyEventBinder.class);
+	
+	public Controller() {
+		eventBus = new SimpleEventBus();
+		eventBinder.bindEventHandlers(this, eventBus);
+		mainView = new MainView();
+		
+		RootLayoutPanel.get().add(mainView);
+	}
 }
