a009571be4fadf8d1d68a2ab636d98737ea15772
Blumen <rasmus_blumensaadt@hotmail.com>
2016-03-15 12:12:02 +0100

    servlets oprettet

--- src/dtu/rejseafregning/client/services/IMedarbejderDAO.java
+++ src/dtu/rejseafregning/client/services/IMedarbejderDAO.java
@@ -3,12 +3,13 @@ package dtu.rejseafregning.client.services;
 import java.util.List;
 
 import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 import dtu.rejseafregning.shared.DALException;
 import dtu.rejseafregning.shared.MedarbejderDTO;
 
+@RemoteServiceRelativePath("medarbejderservice")
 public interface IMedarbejderDAO extends RemoteService{
-
 	MedarbejderDTO getMedarbejder(String Brugernavn) throws DALException;
 	List<MedarbejderDTO> getMedarbejderList() throws DALException;
 	void createMedarbejder(MedarbejderDTO medarbejder) throws DALException;
