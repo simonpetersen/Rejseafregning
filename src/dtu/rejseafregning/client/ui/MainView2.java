8d00576d76d688d455f7a72186b850b6278d1a3d
Simon <simon_petersen1994@hotmail.com>
2016-04-10 21:59:04 +0200

    Tilføjet DeckPanel til MainView.

--- src/dtu/rejseafregning/client/ui/MainView2.java
+++ src/dtu/rejseafregning/client/ui/MainView2.java
@@ -5,19 +5,18 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DeckPanel;
 import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.web.bindery.event.shared.EventBus;
 import com.google.web.bindery.event.shared.binder.EventBinder;
 
 import dtu.rejseafregning.client.events.LogudButtonEvent;
+import dtu.rejseafregning.shared.MedarbejderDTO;
 
 public class MainView2 extends Composite {
 
@@ -25,7 +24,10 @@ public class MainView2 extends Composite {
 	@UiField Button btnLogud;
 	@UiField HorizontalPanel hPanel;
 	@UiField VerticalPanel vPanel1;
-	@UiField VerticalPanel vPanel2;
+	@UiField DeckPanel contentPanel;
+	
+	private MedarbejderDTO bruger;
+	private OplysningerView oplysningerView;
 	
 	private final EventBus eventBus;
 	
@@ -39,6 +41,9 @@ public class MainView2 extends Composite {
 		
 		this.eventBus = eventBus;
 		eventBinder.bindEventHandlers(this,eventBus);
+		
+		bruger = new MedarbejderDTO("SIMPE", "", "");
+		oplysningerView = new OplysningerView(eventBus, bruger);
 	
 		initWidget(uiBinder.createAndBindUi(this));
 	}
@@ -58,6 +63,6 @@ public class MainView2 extends Composite {
 	
 	@UiHandler("mineoplysninger")
 	void onMineoplysningerClick(ClickEvent event) {
-		Window.alert("Der er valgt Mine oplysninger");
+		contentPanel.showWidget(0);
 	}
 }
