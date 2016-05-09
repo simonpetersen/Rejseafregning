f73ce017529967acd28407af7f73020407a1dc1c
Blumen <rasmus_blumensaadt@hotmail.com>
2016-05-09 10:03:56 +0200

    api tests

--- src/RESTFul/OpenAPI.java
+++ src/RESTFul/OpenAPI.java
@@ -6,6 +6,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import dtu.rejseafregning.server.dal.MedarbejderDAO;
+import dtu.rejseafregning.shared.MedarbejderDTO;
+
 @Path("/antal")
 public class OpenAPI {
 
@@ -17,14 +20,23 @@ public class OpenAPI {
 		fahrenheit = ((celsius * 9) / 5) + 32;
  
 		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
-		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "<infoTilSimon>" + "Simon har en langsom computer" + "</infoTilSimon>" + "</ctofservice>";
 	}
 	
 	@Path("{c}")
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getTal(@PathParam("c") int number){
-		return "Du har valgt dette tal: " + number;
+	public String getTal(@PathParam("c") String studienr){
+		String navn = "";
+		try {
+			MedarbejderDAO medarbejder = new MedarbejderDAO();
+			navn = medarbejder.getMedarbejder(studienr).getNavn();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "Du har valgt denne studerende: " + studienr + "\n" + "Personen hedder: " + navn;
 	}
 	
 }
