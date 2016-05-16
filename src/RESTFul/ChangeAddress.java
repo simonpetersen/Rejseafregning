dad3e3c0dbb2b63047e142e60bef1a45970399ae
Blumen <rasmus_blumensaadt@hotmail.com>
2016-05-16 10:34:51 +0200

    ChangeAddress - kan nu tage forskellige antal af argumenter

--- src/RESTFul/ChangeAddress.java
+++ src/RESTFul/ChangeAddress.java
@@ -51,6 +51,93 @@ public class ChangeAddress {
 		}
 		return resultat;
 	}
+	
+	@PUT
+	@Path("{user}/{pass}/{postnr}/{vejnavn}/{husnr}")
+	@Consumes("application/x-www-form-urlencoded")
+	@Produces("text/plain")
+	public String UpdateAddress(@PathParam("user") final String user, @PathParam("pass") String pass,
+			@PathParam("postnr") final String postnr, @PathParam("vejnavn") final String vejnavn,
+			@PathParam("husnr") final String husnr) throws Exception {
+
+		try {
+			MedarbejderDAO medarbejderDAO = new MedarbejderDAO();
+			MedarbejderDTO medarbejder = medarbejderDAO.login(user, pass);
+			String gammelAdresse = medarbejder.getVejnavn() + " " + medarbejder.getHusnr() + " "
+					+ medarbejder.getEtage() + " " + medarbejder.getDoer() + "\n" + medarbejder.getPostnr();
+			medarbejder.setPostnr(postnr);
+			medarbejder.setVejnavn(vejnavn);
+			medarbejder.setHusnr(husnr);
+			medarbejder.setEtage("");
+			medarbejder.setDoer("");
+
+			medarbejderDAO.updateMedarbejder(medarbejder);
+			resultat = "Medarbejderen: " + user + " blev opdateret." + "\n" + "Den gamle adresse var: " + gammelAdresse
+					+ "\n" + "\n" + "Adressen er nu: " + vejnavn + " " + husnr + "\n"
+					+ postnr;
+		} catch (DALException e) {
+			resultat = "Der kunne ikke logges ind p� brugeren. Tjek at oplysningerne er korrekte og pr�v igen.";
+		}
+		return resultat;
+	}
+	
+	@PUT
+	@Path("{user}/{pass}/{postnr}/{vejnavn}/{husnr}/{etage}")
+	@Consumes("application/x-www-form-urlencoded")
+	@Produces("text/plain")
+	public String UpdateAddressEtage(@PathParam("user") final String user, @PathParam("pass") String pass,
+			@PathParam("postnr") final String postnr, @PathParam("vejnavn") final String vejnavn,
+			@PathParam("husnr") final String husnr, @PathParam("etage") final String etage) throws Exception {
+
+		try {
+			MedarbejderDAO medarbejderDAO = new MedarbejderDAO();
+			MedarbejderDTO medarbejder = medarbejderDAO.login(user, pass);
+			String gammelAdresse = medarbejder.getVejnavn() + " " + medarbejder.getHusnr() + " "
+					+ medarbejder.getEtage() + " " + medarbejder.getDoer() + "\n" + medarbejder.getPostnr();
+			medarbejder.setPostnr(postnr);
+			medarbejder.setVejnavn(vejnavn);
+			medarbejder.setHusnr(husnr);
+			medarbejder.setEtage(etage);
+			medarbejder.setDoer("");
+
+			medarbejderDAO.updateMedarbejder(medarbejder);
+			resultat = "Medarbejderen: " + user + " blev opdateret." + "\n" + "Den gamle adresse var: " + gammelAdresse
+					+ "\n" + "\n" + "Adressen er nu: " + vejnavn + " " + husnr + " " + etage + "\n"
+					+ postnr;
+		} catch (DALException e) {
+			resultat = "Der kunne ikke logges ind p� brugeren. Tjek at oplysningerne er korrekte og pr�v igen.";
+		}
+		return resultat;
+	}
+	
+	@PUT
+	@Path("{user}/{pass}/{postnr}/{vejnavn}/{husnr}/{doer}")
+	@Consumes("application/x-www-form-urlencoded")
+	@Produces("text/plain")
+	public String UpdateAddressDoor(@PathParam("user") final String user, @PathParam("pass") String pass,
+			@PathParam("postnr") final String postnr, @PathParam("vejnavn") final String vejnavn,
+			@PathParam("husnr") final String husnr,@PathParam("doer") final String doer) throws Exception {
+
+		try {
+			MedarbejderDAO medarbejderDAO = new MedarbejderDAO();
+			MedarbejderDTO medarbejder = medarbejderDAO.login(user, pass);
+			String gammelAdresse = medarbejder.getVejnavn() + " " + medarbejder.getHusnr() + " "
+					+ medarbejder.getEtage() + " " + medarbejder.getDoer() + "\n" + medarbejder.getPostnr();
+			medarbejder.setPostnr(postnr);
+			medarbejder.setVejnavn(vejnavn);
+			medarbejder.setHusnr(husnr);
+			medarbejder.setEtage("");
+			medarbejder.setDoer(doer);
+
+			medarbejderDAO.updateMedarbejder(medarbejder);
+			resultat = "Medarbejderen: " + user + " blev opdateret." + "\n" + "Den gamle adresse var: " + gammelAdresse
+					+ "\n" + "\n" + "Adressen er nu: " + vejnavn + " " + husnr + " " + doer + "\n"
+					+ postnr;
+		} catch (DALException e) {
+			resultat = "Der kunne ikke logges ind p� brugeren. Tjek at oplysningerne er korrekte og pr�v igen.";
+		}
+		return resultat;
+	}
 
 	@GET
 	@Path("{user}/{pass}")
